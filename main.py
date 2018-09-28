7e1a32e763100241c390a7b18ff1b2db929045bc
Sergey Bugaev <bugaevc@gmail.com>
2018-09-28 21:35:45 +0300

    Parse the request and start sending 404s

--- main.py
+++ main.py
@@ -8,13 +8,28 @@ s.listen()
 while True:
     s2, client_addr = s.accept()
 
-    s2.send(b'HTTP/1.0 200 OK\r\n')
+    request = s2.recv(2048).decode().splitlines()
+    first_line = request.pop(0)
+    # METHOD /path HTTP/version
+    method, path, http_version = first_line.split()
+    http_version = http_version[len('HTTP/'):]
+
+    if path == '/':
+        code = 200
+        code_name = 'OK'
+        body = 'hello world!'
+    else:
+        code = 404
+        code_name = 'Not Found'
+        body = 'not found'
+
+    s2.send('HTTP/1.0 {} {}\r\n'.format(code, code_name).encode())
 
     s2.send(b'Server: Simple HTTP server 0.1\r\n')
     s2.send(b'Content-Type: text/plain\r\n')
 
     s2.send(b'\r\n')
 
-    s2.send(b'hello world\n')
+    s2.send(body.encode())
 
     s2.close()
