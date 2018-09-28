2ed4d0a31582c27aaf2ced57fbdb11a1e5355df3
Sergey Bugaev <bugaevc@gmail.com>
2018-09-28 21:31:31 +0300

    Send an actual HTTP response

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ s.listen()
 
 while True:
     s2, client_addr = s.accept()
+
+    s2.send(b'HTTP/1.0 200 OK\r\n')
+
+    s2.send(b'Server: Simple HTTP server 0.1\r\n')
+    s2.send(b'Content-Type: text/plain\r\n')
+
+    s2.send(b'\r\n')
+
     s2.send(b'hello world\n')
+
     s2.close()
