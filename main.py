b89a5b2fd37a86b2b05f538d3bf207c17b7eaac0
Sergey Bugaev <bugaevc@gmail.com>
2018-09-28 22:52:23 +0300

    Introduce Server & ToDo classes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import socket
 
-s = socket.socket()
-server_addr = '127.0.0.1', 8000
-s.bind(server_addr)
-s.listen()
+
+class NoHandlerError(Exception):
+    pass
 
 
 class Request:
@@ -18,87 +17,29 @@ class Handler:
         raise RuntimeError("abstract")
 
 
-todo_list = []
-handlers = []
+class Server:
+    def __init__(self):
+        self.s = socket.socket()
+        server_addr = '127.0.0.1', 8000
+        self.s.bind(server_addr)
+        self.handlers = []
 
+    def add_handler(self, handler):
+        self.handlers.append(handler)
 
-class RootHandler(Handler):
-    def can_handle(self, request):
-        return request.path == '/' and request.method == 'GET'
+    def serve_forever(self):
+        self.s.listen()
+        while True:
+            s2, client_addr = self.s.accept()
+            try:
+                self.handle_client(s2)
+            except:
+                s2.send(b'HTTP/1.0 500 Internal Server Error\r\n')
+                s2.send(b'\r\n')
+            finally:
+                s2.close()
 
-    def handle(self, request):
-        return '''
-                <html>
-                    <head>
-                        <title>To-do list</title>
-                    </head>
-                    <body>
-                        <p>Your to-do list:</p>
-                        <ul>
-                            {}
-                        </ul>
-                        <form action="/new" method="post">
-                            <p>Add a new item:</p>
-                            <input type="text" name="name" placeholder="Do stuff"/>
-                            <input type="submit" value="Add"/>
-                        </form>
-                    </body>
-                </html>'''.format('\n'.join('<li>' + item + '</li>' for item in todo_list))
-
-
-handlers.append(RootHandler())
-
-
-class NewHandler(Handler):
-    def can_handle(self, request):
-        return request.path == '/new' and request.method == 'POST'
-
-    def handle(self, request):
-        new_todo = request.body.strip()
-        new_todo = new_todo[len("name="):].replace('+', ' ')
-        todo_list.append(new_todo)
-        return 201, '<html><body>Created! Go back to the <a href="/">frontpage</a>.</body></html>'
-
-
-handlers.append(NewHandler())
-
-
-class FourOhFourHandler(Handler):
-    def can_handle(self, request):
-        return True
-
-    def handle(self, request):
-        return 404, '<html><body><font color="red">Not Found</font></body></html>'
-
-
-handlers.append(FourOhFourHandler())
-
-
-class NoHandlerError(Exception):
-    pass
-
-
-def handle_request(request):
-    for handler in handlers:
-        if handler.can_handle(request):
-            return handler.handle(request)
-    raise NoHandlerError()
-
-
-def parse_headers(raw_request):
-    headers = dict()
-    while True:
-        line = raw_request.pop(0)
-        if not line:
-            # reached the end of headers
-            break
-        name, colon, value = line.partition(': ')
-        headers[name] = value
-    return headers
-
-
-def handle_client(client):
-    try:
+    def handle_client(self, client):
         raw_request = client.recv(2048).decode().splitlines()
         request = Request()
         first_line = raw_request.pop(0)
@@ -106,10 +47,10 @@ def handle_client(client):
         request.method, request.path, request.http_version = first_line.split()
         request.http_version = request.http_version[len('HTTP/'):]
 
-        request.headers = parse_headers(raw_request)
+        request.headers = self.parse_headers(raw_request)
         request.body = '\n'.join(raw_request)
 
-        result = handle_request(request)
+        result = self.handle_request(request)
 
         if isinstance(result, str):
             code = 200
@@ -132,13 +73,91 @@ def handle_client(client):
 
         client.send(b'\r\n')
         client.send(body)
-    except:
-        client.send(b'HTTP/1.0 500 Internal Server Error\r\n')
-        client.send(b'\r\n')
-    finally:
-        client.close()
+
+    def parse_headers(self, raw_request):
+        headers = dict()
+        while True:
+            line = raw_request.pop(0)
+            if not line:
+                # reached the end of headers
+                break
+            name, colon, value = line.partition(': ')
+            headers[name] = value
+        return headers
+
+    def handle_request(self, request):
+        for handler in self.handlers:
+            if handler.can_handle(request):
+                return handler.handle(request)
+        raise NoHandlerError()
+
+class ToDo:
+    def __init__(self):
+        self.list = []
+
+    def render_html(self):
+        res = '<ul>'
+        for item in self.list:
+            res += '<li>' + item + '</li>'
+        res += '</ul>'
+        return res
+
+    def add(self, item):
+        self.list.append(item)
 
 
-while True:
-    s2, client_addr = s.accept()
-    handle_client(s2)
+class RootHandler(Handler):
+    def __init__(self, todo):
+        self.todo = todo
+
+    def can_handle(self, request):
+        return request.path == '/' and request.method == 'GET'
+
+    def handle(self, request):
+        return '''
+                <html>
+                    <head>
+                        <title>To-do list</title>
+                    </head>
+                    <body>
+                        <p>Your to-do list:</p>
+                        {}
+                        <form action="/new" method="post">
+                            <p>Add a new item:</p>
+                            <input type="text" name="name" placeholder="Do stuff"/>
+                            <input type="submit" value="Add"/>
+                        </form>
+                    </body>
+                </html>'''.format(self.todo.render_html())
+
+
+class NewHandler(Handler):
+    def __init__(self, todo):
+        self.todo = todo
+
+    def can_handle(self, request):
+        return request.path == '/new' and request.method == 'POST'
+
+    def handle(self, request):
+        new_todo = request.body.strip()
+        new_todo = new_todo[len("name="):].replace('+', ' ')
+        self.todo.add(new_todo)
+        return 201, '<html><body>Created! Go back to the <a href="/">frontpage</a>.</body></html>'
+
+
+class FourOhFourHandler(Handler):
+    def can_handle(self, request):
+        return True
+
+    def handle(self, request):
+        return 404, '<html><body><font color="red">Not Found</font></body></html>'
+
+
+server = Server()
+todo_list = ToDo()
+
+server.add_handler(RootHandler(todo_list))
+server.add_handler(NewHandler(todo_list))
+server.add_handler(FourOhFourHandler())
+
+server.serve_forever()
